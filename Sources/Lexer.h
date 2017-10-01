9c50d0a057944e2718e32b4d80558a35b5348990
Chris Ohk <utilforever@gmail.com>
2017-10-01 23:37:10 +0900

    Update Lexer.h - Implement Token class

--- Sources/Lexer.h
+++ Sources/Lexer.h
@@ -1,6 +1,8 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#include <cassert>
+
 enum class Symbol
 {
 	LETTER,
@@ -80,6 +82,82 @@ enum class TokenType
 
 	// Not really token
 	UNKNOWN
+};
+
+struct Token
+{
+	// Constructors
+	Token(TokenType _token = TokenType::ERROR) : token(_token)
+	{
+		val.numVal = 0;
+	}
+
+	Token(TokenType _token, char ch) : token(_token)
+	{
+		val.chVal = ch;
+	}
+
+	Token(TokenType _token, int num) : token(_token)
+	{
+		val.numVal = num;
+	}
+
+	Token(TokenType _token, char* str) : token(_token)
+	{
+		val.strVal = str;
+	}
+
+	Token(TokenType _token, double d) : token(_token)
+	{
+		val.fpVal = d;
+	}
+
+	// Destructor
+	~Token()
+	{
+		if (token == TokenType::STRING_LITERAL ||
+			token == TokenType::NUM_INT ||
+			token == TokenType::NUM_DOUBLE ||
+			token == TokenType::NUM ||
+			token == TokenType::ID)
+		{
+			delete val.strVal;
+		}
+	}
+
+	// Interface for accessing internal data
+	char GetChVal()
+	{
+		assert(token == TokenType::CHAR);
+		return val.chVal;
+	}
+
+	int GetNumVal()
+	{
+		assert(token == TokenType::NUM_INT);
+		return val.numVal;
+	}
+
+	char* GetStrVal()
+	{
+		assert(token == TokenType::STRING_LITERAL);
+		return val.strVal;
+	}
+
+	double GetFpVal()
+	{
+		assert(token == TokenType::NUM_DOUBLE);
+		return val.fpVal;
+	}
+
+	TokenType token;
+	union
+	{
+		char chVal;
+		int numVal;
+		char* strVal;
+		double fpVal;
+	} val;
 };
 
 #endif
